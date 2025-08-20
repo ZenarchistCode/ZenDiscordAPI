4921911f7d3d11aab7ce94976fc68c97c6474db1
Zenarchist <epochx.server@gmail.com>
2025-08-20 21:37:03 +1000

    Update

--- scripts/4_world/ZenDiscordAPI/entities/ZenRaidAlarm/itembase/Zen_RaidAlarmStationKit.c
+++ scripts/4_world/ZenDiscordAPI/entities/ZenRaidAlarm/itembase/Zen_RaidAlarmStationKit.c
@@ -10,26 +10,6 @@ class Zen_RaidAlarmStationKit extends ZenKitBoxBase_RaidAlarm
 		return "0 0 0";
 	}
 
-	override void OnPlacementComplete(Man player, vector position = "0 0 0", vector orientation = "0 0 0")
-	{
-		super.OnPlacementComplete(player, position, orientation);
-
-		if (!GetGame().IsDedicatedServer())
-			return;
-
-		PlayerBase pb = PlayerBase.Cast(player);
-		if (!pb)
-			return;
-		
-		Zen_RaidAlarmStation station = Zen_RaidAlarmStation.Cast(GetGame().CreateObject(GetDeployedClassname(), pb.GetLocalProjectionPosition(), false));
-		if (!station)
-			return;
-
-		SetIsDeploySound(true);
-		station.SetPosition(position);
-		station.SetOrientation(orientation);
-	}
-
 	override void SetActions()
 	{
 		super.SetActions();
